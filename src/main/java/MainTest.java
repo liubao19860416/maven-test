f91bfdb2e73500c440ad3b6063e59789c1655db3
liubao <18611478781@163.com>
2016-08-28 10:57:11 +0800

    初始化本地提交

---
+++ src/main/java/MainTest.java
@@ -0,0 +1,13 @@
+/**
+ * 主方法测试类
+ * 
+ * @author Liubao
+ * @2016年8月28日
+ * 
+ */
+public class MainTest {
+
+    public static void main(String[] args) {
+        System.out.println("Main method 执行了...");
+    }
+}
